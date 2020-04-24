99b9beccc7eb34ed2ff95bd05fe9868e57c9cc7f
Adriel <adrielarandez@gmail.com>
2020-04-24 01:52:12 -0300

    Restructuración

--- Colecciones/LISTA.cs
+++ Colecciones/LISTA.cs
@@ -5,8 +5,129 @@ using System.Text;
 
 namespace Colecciones
 {
-    public class LISTA
+    public class LISTA<T>
     {
+        private int cantidad;
 
-    }
+        public int Cantidad
+        {
+            get { return cantidad; }
+            set { cantidad = value; }
+        }
+
+        private NODO<T> primerNodo;
+
+        public NODO<T> PrimerNodo
+        {
+            get { return primerNodo; }
+            set { primerNodo = value; }
+        }
+
+        private NODO<T> nodoActual;
+
+        public NODO<T> NodoActual
+        {
+            get { return nodoActual; }
+            set { nodoActual = value; }
+        }
+
+
+        private NODO<T> ultimoNodo;
+
+        public NODO<T> UltimoNodo
+        {
+            get { return ultimoNodo; }
+            set { ultimoNodo = value; }
+        }
+
+
+        public void AgregarPrimero(NODO<T> nuevoNodo)
+        {
+            if (cantidad <= 0)
+            {
+                nuevoNodo.ProximoNodo = null;
+                primerNodo = nuevoNodo;
+                ultimoNodo = nuevoNodo;
+                cantidad++;
+            }
+            else
+            {
+                nuevoNodo.Posicion = primerNodo.Posicion;
+                nuevoNodo.ProximoNodo = primerNodo;
+                nodoActual = primerNodo;
+                for(int i = 0; i < cantidad; i++)
+                {
+                    nodoActual.Posicion++;
+                    if (nodoActual.ProximoNodo != null)
+                    {
+                        nodoActual = nodoActual.ProximoNodo;
+                    }
+                }
+                primerNodo = nuevoNodo;
+                cantidad++;
+
+            }
+        }
+
+        public void AgregarUltimo(NODO<T> nuevoNodo)
+        {
+            if (cantidad<=0)
+            {
+                nuevoNodo.ProximoNodo = null;
+                nuevoNodo.Posicion = cantidad;
+                primerNodo = nuevoNodo;
+                ultimoNodo = nuevoNodo;
+                cantidad++;
+            }
+            else
+            {
+                nuevoNodo.ProximoNodo = null;
+                nuevoNodo.Posicion = cantidad;
+                ultimoNodo.ProximoNodo = nuevoNodo;
+                ultimoNodo = nuevoNodo;
+                cantidad++;
+            }
+        }
+
+
+        public NODO<T> Obtener(int index)
+        {
+            NODO<T> nodoPedido=null;
+
+            nodoActual = primerNodo;
+            for (int i = 0; i < cantidad; i++)
+            {
+                if (nodoActual.Posicion == index)
+                {
+                    nodoPedido = nodoActual;
+                    //return nodoPedido;
+                }
+                else
+                {
+                    nodoActual = nodoActual.ProximoNodo;
+                }
+            }
+
+            return nodoPedido;
+        }
+
+        public NODO<T> ObtenerPrimero(int index)
+        {
+            NODO<T> nodoPedido = null;
+            nodoPedido = primerNodo;
+            return nodoPedido;
+        }
+
+        public NODO<T> ObtenerUltimo(int index)
+        {
+            NODO<T> nodoPedido = null;
+            nodoPedido = ultimoNodo;
+            return nodoPedido;
+        }
+
+        public int Contar()
+        {
+            return cantidad;
+        }
+    }    
 }
---
+++ Colecciones/NODO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Colecciones
+{
+    public class NODO<T>
+    {
+        private int posicion;
+
+        public int Posicion
+        {
+            get { return posicion; }
+            set { posicion = value; }
+        }
+
+        private T valor;
+
+        public T Valor
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+
+        private NODO<T> proximoNodo;
+
+        public NODO<T> ProximoNodo
+        {
+            get { return proximoNodo; }
+            set { proximoNodo = value; }
+        }
+
+    }
+}
